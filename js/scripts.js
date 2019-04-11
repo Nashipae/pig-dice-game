a98882bfea71c02ec62b4edb4a75f292c9aea3ab
nwariara <nwariara@safaricom.co.ke>
2019-04-11 19:11:43 +0300

    add basic html file.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var listItems = document.getElementById("our-list").getElementsByTagName("li");
+var p1=0;
+var h1= 0
+// for(i=0; i<listItems.length; i++){
+//   listItems[i].addEventListener("click", activateItem);
+// }
+// function activateItem(){
+//   // alert("Click detected.");
+//   p1+=1
+//   console.log(p1);
+//   h1+=p1
+  
+// }
