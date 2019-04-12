6576f006edcb9426aeeb347cc6b6594c93127e9c
nwariara <nwariara@safaricom.co.ke>
2019-04-12 19:15:36 +0300

    add if statement.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,5 @@
 var rotateDie = function () {
   return Math.floor(Math.random() * 6) + 1;
-
 }
 
 function player() {
@@ -15,15 +14,26 @@ var player2 = new player();
 
 player.prototype.rollDice = function (){
   this.roll = rotateDie();
-  this.score.push(this.roll);
+  if (this.roll ===1){
+    this.score=[];
+    return this.ptotal;
+  } 
+  else {
+    this.score.push(this.roll);
+  }
 }
 player.prototype.holdDice = function(){
   this.ptotal = this.ptotal + this.score.reduce(function (total, num) {
     return (total + num);})
+  return this.ptotal;
 }
-function print1(){
-  player1.rollDice();
-  player1.holdDice();
-  console.log(player1.ptotal);
-}
+// function print1(){
+//   player1.rollDice();
+//   player1.holdDice();
+//   console.log(player1.ptotal);
+// }
 
+// User Logic
+$(document).ready(function () {
+  
+}); 
