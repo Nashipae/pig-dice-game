e89f52a821d41508e59329a94c9df4305f318967
nwariara <nwariara@safaricom.co.ke>
2019-04-12 18:53:22 +0300

    develop prototype further.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,24 +1,29 @@
 var rotateDie = function () {
-  Math.floor((Math.random() * 6) + 1);
+  return Math.floor(Math.random() * 6) + 1;
+
 }
 
 function player() {
   this.roll = 0;
   this.hold = 0;
   this.score = [];
-  this.ptotal = [];
+  this.ptotal = 0;
 }
 
 var player1 = new player();
-var player2=new player();
+var player2 = new player();
 
 player.prototype.rollDice = function (){
   this.roll = rotateDie();
   this.score.push(this.roll);
 }
 player.prototype.holdDice = function(){
-  this.ptotal = this.ptotal + (this.score.reduce(function getSum(total, num) {
-    return total + num;}))
+  this.ptotal = this.ptotal + this.score.reduce(function (total, num) {
+    return (total + num);})
 }
-
+function print1(){
+  player1.rollDice();
+  player1.holdDice();
+  console.log(player1.ptotal);
+}
 
